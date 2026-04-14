72dad67327409e60ec69bbccb61d694fbacac3e8
Satya <satyadwibedy42@gmail.com>
2026-04-14 10:18:50 +0530

    add authentication system with session management, secure routes, career path module, and API integration for roadmap and user tracking

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request,jsonify, session
+from flask import Flask, render_template, request,jsonify, session ,redirect
 from predict import predict_career
 from quiz_data import get_random_questions
+from career_path_data import career_paths
 
 
 app = Flask(__name__)
@@ -14,6 +15,8 @@ def home():
 
 @app.route("/dashboard")
 def dashboard():
+    if "user_id" not in session:
+        return redirect("/login")
     return render_template ('dashboard.html')
 
 
@@ -29,6 +32,8 @@ def register():
 
 @app.route('/form')
 def form():
+    if "user_id" not in session:
+        return redirect("/login")
     return render_template('form.html')
 
 @app.route('/predict', methods=['POST'])
@@ -47,11 +52,15 @@ def predict():
 
 @app.route('/result')
 def result():
+    if "user_id" not in session:
+        return redirect("/login")
     career = session.get('career')
     return render_template('result.html', career=career)
 
 @app.route("/quiz")
 def quiz_page():
+    if "user_id" not in session:
+        return redirect("/login")
     career = request.args.get("career")
     return render_template("quiz.html", career=career)
 
@@ -59,8 +68,31 @@ def quiz_page():
 @app.route("/get-quiz/<career>")
 def get_quiz(career):
     return jsonify(get_random_questions(career))
+
+    
+@app.route("/career-path")
+def career_path():
+    if "user_id" not in session:
+        return redirect("/login")
+    return render_template("career_path.html")
+
+
+@app.route("/get-path/<career>")
+def get_path(career):
+    return jsonify(career_paths.get(career, {}))
+
+
+@app.route("/set-user", methods=["POST"])
+def set_user():
+    data = request.json
+    session["user_id"] = data["user_id"]
+    return jsonify({"message": "Session set"})
   
 
+@app.route("/logout")
+def logout():
+    session.clear()
+    return jsonify({"message": "Logged out"}) 
 
 
 
