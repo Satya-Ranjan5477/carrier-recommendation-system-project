8ee2c50a91ddfd0fd28b9985bbaacec92d211fc3
Tejaswini <tejaswinibhuyan30@gmail.com>
2026-04-16 08:44:48 +0530

    Extend model training to include traits using MultiLabelBinarizer

--- train_model.py
+++ train_model.py
@@ -9,13 +9,16 @@ df = pd.read_csv("career_data.csv")
 # Convert to list
 df['skills'] = df['skills'].apply(lambda x: x.split(','))
 df['interests'] = df['interests'].apply(lambda x: x.split(','))
+df['traits'] = df['traits'].apply(lambda x: x.split(','))
 
 # Encode
 mlb_skills = MultiLabelBinarizer()
 mlb_interests = MultiLabelBinarizer()
+mlb_traits = MultiLabelBinarizer()
 
 skills_encoded = mlb_skills.fit_transform(df['skills'])
 interests_encoded = mlb_interests.fit_transform(df['interests'])
+traits_encoded = mlb_traits.fit_transform(df['traits'])
 
 le_q = LabelEncoder()
 df['qualification'] = le_q.fit_transform(df['qualification'])
@@ -24,7 +27,7 @@ le_c = LabelEncoder()
 df['career'] = le_c.fit_transform(df['career'])
 
 # Combine
-X = np.hstack((df[['qualification']].values, skills_encoded, interests_encoded))
+X = np.hstack((df[['qualification']].values, skills_encoded, interests_encoded,traits_encoded))
 y = df['career']
 
 model = RandomForestClassifier()
@@ -36,5 +39,7 @@ joblib.dump(le_q, "le_q.pkl")
 joblib.dump(le_c, "le_c.pkl")
 joblib.dump(mlb_skills, "mlb_skills.pkl")
 joblib.dump(mlb_interests, "mlb_interests.pkl")
+joblib.dump(mlb_traits, "mlb_traits.pkl")
+
 
 print("Model trained successfully!")
