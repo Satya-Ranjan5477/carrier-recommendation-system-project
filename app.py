363c040dce1e94251e5b320f2a597560b70241aa
Satya <satyadwibedy42@gmail.com>
2026-07-04 05:43:26 +0530

    fix firebase auth issue

--- app.py
+++ app.py
@@ -160,9 +160,17 @@ def logout():
     return redirect("/login")
 
 
+@app.route("/debug-env")
+def debug_env():
+    return {
+        "apiKey": os.getenv("FIREBASE_API_KEY"),
+        "authDomain": os.getenv("FIREBASE_AUTH_DOMAIN"),
+        "projectId": os.getenv("FIREBASE_PROJECT_ID"),
+        "storageBucket": os.getenv("FIREBASE_STORAGE_BUCKET"),
+        "messagingSenderId": os.getenv("FIREBASE_MESSAGING_SENDER_ID"),
+        "appId": os.getenv("FIREBASE_APP_ID")
+    }
 
-'''if __name__ == "__main__":
-    app.run(debug=True)'''
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
